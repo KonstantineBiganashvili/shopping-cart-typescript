e338d0d10ec6603723ba5fa560a2aa3396c842b3
KonstantineBiganashvili <konstantinebiganashvili@gmail.com>
2022-11-17 12:46:03 +0400

    finished app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,102 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
+import { Drawer, LinearProgress, Grid, Badge } from '@mui/material';
+import { AddShoppingCart } from '@mui/icons-material';
+import { StyledButton, Wrapper } from './App.styles';
+import Item from './item';
+import Cart from './Cart';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+const getProducts = async (): Promise<CartItemType[]> => {
+  return await (await fetch('https://fakestoreapi.com/products')).json();
+};
+
+const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products',
+    getProducts
   );
-}
+
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
+
+  const getTotalItems = (items: CartItemType[]) => {
+    return items.reduce((acc: number, item) => {
+      return acc + item.amount;
+    }, 0);
+  };
+
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems((oldCartItems) => {
+      const isItemInCart = oldCartItems.find(
+        (item) => item.id === clickedItem.id
+      );
+
+      if (isItemInCart) {
+        return oldCartItems.map((item) =>
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        );
+      }
+
+      return [...oldCartItems, { ...clickedItem, amount: 1 }];
+    });
+  };
+
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems((oldCartItems) => {
+      return oldCartItems.reduce((acc, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) {
+            return acc;
+          }
+
+          return [...acc, { ...item, amount: item.amount - 1 }];
+        } else {
+          return [...acc, item];
+        }
+      }, [] as CartItemType[]);
+    });
+  };
+
+  if (isLoading) return <LinearProgress />;
+
+  if (error) return <div>Something Went Wrong</div>;
+
+  return (
+    <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
+        <Cart
+          cartItems={cartItems}
+          addToCart={handleAddToCart}
+          removeFromCart={handleRemoveFromCart}
+        />
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color="error">
+          <AddShoppingCart />
+        </Badge>
+      </StyledButton>
+      <Grid container spacing={3}>
+        {data?.map((item: CartItemType) => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  );
+};
 
 export default App;
---
+++ src/item/index.tsx
@@ -0,0 +1,27 @@
+import { Button } from '@mui/material';
+import React, { FC } from 'react';
+import { CartItemType } from '../App';
+import { Wrapper } from './Item.styles';
+
+type Props = {
+  item: CartItemType;
+  handleAddToCart: (clickedItem: CartItemType) => void;
+};
+
+const Item: FC<Props> = (props) => {
+  const { item, handleAddToCart } = props;
+
+  return (
+    <Wrapper>
+      <img src={item.image} alt={item.title} />
+      <div>
+        <h3>{item.title}</h3>
+        <p>{item.description}</p>
+        <h3>${item.price}</h3>
+      </div>
+      <Button onClick={() => handleAddToCart(item)}>Add to cart</Button>
+    </Wrapper>
+  );
+};
+
+export default Item;
